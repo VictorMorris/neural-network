a88f658057a2a425c4fd70e68e99de70881b917d
Victor Morris <57648247+VictorMorris@users.noreply.github.com>
2026-03-06 10:59:11 -0500

    Achieved Sentience

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import random
 import tkinter as tk
 from tkinter import ttk
+import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -9,6 +10,7 @@ from network import Network
 from readdata import MnistDataloader
 
 
+# Locations of data
 TRAINING_IMAGES = 'input/train-images.idx3-ubyte'
 TRAINING_LABELS = 'input/train-labels.idx1-ubyte'
 TEST_IMAGES     = 'input/t10k-images.idx3-ubyte'
@@ -22,8 +24,10 @@ class App:
         self.y_train = y_train
         self.network = network
         self.current_index = 500
+        self.num_correct = 0
+        self.num_guess = 0
 
-        root.title("VictorGPT 0.1")
+        root.title("VictorNet 0.1")
         root.geometry("450x700")
 
         mainframe = ttk.Frame(root, padding="10 10 10 10")
@@ -42,10 +46,14 @@ class App:
         buttons_frame = ttk.Frame(mainframe)
         buttons_frame.grid(row=1, column=0, pady=5)
 
-        tk.Button(buttons_frame, text="New Image", command=self.new_image).grid(row=0, column=0, padx=5)
+        tk.Button(buttons_frame, text="New Image", command=self.new_image).grid(row=0, column=0, padx=5, sticky="ew")
+        buttons_frame.columnconfigure(0, weight=1)
 
         self.prediction_label = tk.Label(mainframe, text="", font=("Arial", 14))
         self.prediction_label.grid(row=2, column=0, pady=5)
+
+        self.result_label = tk.Label(mainframe, text="", font=("Arial", 14))
+        self.result_label.grid(row=3, column=0, pady=5)
 
     def show_image(self):
         image = self.x_train[self.current_index]
@@ -53,7 +61,7 @@ class App:
         self.ax.clear()
         self.ax.imshow(image.reshape(28, 28), cmap=plt.cm.gray)
         if label != '':
-            self.ax.set_title(f"This is a {label}", fontsize=15)
+            self.ax.set_title(f"Label: {label}", fontsize=15)
 
     def new_image(self):
         self.current_index = random.randint(0, len(self.x_train) - 1)
@@ -65,8 +73,16 @@ class App:
         activations = self.network.feed_forward(self.x_train[self.current_index])
         print(activations)
         guess = list(activations).index(max(activations))
+        if guess == self.y_train[self.current_index]:
+            self.num_correct += 1
+        self.num_guess += 1
         self.prediction_label.config(text=f"Network thinks: {guess}")
+        self.result_label.config(text=f"{self.num_correct}/{self.num_guess}")
 
+def _quit(root):
+    """Function to be called when the window is closed."""
+    root.quit()     # Stops the mainloop
+    root.destroy()  # Destroys the window and all widgets
 
 def main():
     loader = MnistDataloader(TRAINING_IMAGES, TRAINING_LABELS, TEST_IMAGES, TEST_LABELS)
@@ -74,7 +90,15 @@ def main():
 
     network = Network([784, 16, 16, 10])
 
+    def to_one_hot(label):
+        v = np.zeros((10,1))
+        v[label] = 1.0
+        return v
+    training_data = [(x, to_one_hot(y)) for x, y in zip(x_train, y_train)]
+    network.SGD(training_data, 30, 10, 3.0)
+
     root = tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", lambda: _quit(root))
     App(root, x_train, y_train, network)
     root.mainloop()
 
--- src/network.py
+++ src/network.py
@@ -6,17 +6,104 @@ class Network:
     def __init__(self, sizes: list[int]) -> None:
         self.num_layers = len(sizes)
         self.sizes = sizes
+
+        # A layer of biases is stored for every layer except the input layer
+        # Each layer of biases are stored in a column matrix
         self.biases = [np.random.default_rng().standard_normal(size=(y, 1)) for y in sizes[1:]]
+
+        # A matrix of weights is stored for every neuron connection between every layer
+        # Has a row for every neuron in the following layer
+        # Has a column for every nueron in the layer
         self.weights = [np.random.default_rng().standard_normal(size=(y, x)) for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feed_forward(self, data):
         a = data
         for w, b in zip(self.weights, self.biases):
+            # The activation of neurons in the next layer is calculated by:
+            # Weights (size of layer)
             a = self.sigmoid(w @ a + b)
+        # The final activations of the network
+        # A column matrix of length sizes[-1] (One activation for every neuron in the output layer)
         return a
+    
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        """Stocastic Gradient Descent"""
+        if test_data: n_test = len(test_data)
+        n = len(training_data) # Total samples
+        for j in range(epochs):
+            random.shuffle(training_data) # Shuffle is to break ordering bias
+            # Slices training data into batches of size mini_batch_size
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta) # One gradient step per batch
+            if test_data:
+                print(f"Epoch {j}: {self.evaluate(test_data)} / {n_test}")
+            else:
+                print(f"Epoch {j} complete")
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Apply one gradient descent step"""
+        # These will accumulate the total gradient across the mini-batch
+        nabla_b = [np.zeros(b.shape) for b in self.biases] # List of zero arrays, same shape as self.biases
+        nabla_w = [np.zeros(w.shape) for w in self.weights] # List of zero arrays, same shape as self.weights
+        for x, y in mini_batch: # Loop each sample
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y) # Get gradients for this sample
+            # Accumulates the gradients
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        # At this point nabla_b and nabla_w hold the sum of the gradiants from every sample
+        # eta / len(mini_batch) gets the average gradient by dividing by batch size
+        # Subtract the gradient for each weight and bias matrices
+        # Gradient descent update: w = w - eta * gradient
+        self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        """Compute the exact gradient of the loss"""
+        # These will accumulate the total gradient across the mini-batch
+        nabla_b = [np.zeros(b.shape) for b in self.biases] # List of zero arrays, same shape as self.biases
+        nabla_w = [np.zeros(w.shape) for w in self.weights] # List of zero arrays, same shape as self.weights
+        
+        # Forward pass
+        activation = x # Start with the input
+        activations = [x] # Store all the layer activations - needed for backward pass
+        zs = [] # Store all the pre-activation values (z=w*a + b)
+        for b, w in zip(self.biases, self.weights):
+            z = w @ activation + b #Pre activation
+            zs.append(z) # Save the z for this layer
+            activation = self.sigmoid(z)
+            activations.append(activation) # Save the activation for this layer
+
+        # Backward pass
+        # Tells how much each output neuron contributed to the error
+        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta # Output layer bias gradient
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose()) # Output layer weight gradient
+        
+        # Propagate the rror signal backward using the chain rule
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = self.sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp # Propagate delta backward
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+    
+    def evaluate(self, test_data):
+        """Accuracy check"""
+        test_results = [(np.argmax(self.feed_forward(x)), y) for (x,y) in test_data]
+        return sum(int(x==y) for (x,y) in test_results) # Count correct predictions
+    
+    def cost_derivative(self, output_activations, y):
+        """Computes the derivative of the mean squared error loss"""
+        return(output_activations-y)
+    
 
     def sigmoid(self, z):
         return 1.0 / (1.0 + np.exp(-z))
+
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z)*(1-self.sigmoid(z))
 
     def print_biases(self):
         layer = 0
